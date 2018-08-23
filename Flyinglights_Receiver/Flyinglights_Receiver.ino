6467bc09e7506b98e18c7fe771f5370ba6df9e52
MalcM <malcolm@morleys.net>
2018-08-23 16:10:31 +0100

    CUSTOM BITS AT THE START

--- Flyinglights_Receiver/Flyinglights_Receiver.ino
+++ Flyinglights_Receiver/Flyinglights_Receiver.ino
@@ -1,3 +1,65 @@
+
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // CUSTOMISE THE LINES BELOW TO MATCH YOUR SETUP ///////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// for the three lines below it can be worth setting one more
+// than the actual number of pixels if the last pixel is behaving oddly
+#define CTOTALPIXELS 342 //TOTALNUMBER OF PIXELS ON THE CANOPY
+#define STOTALPIXELS 23 //total number of pixels on the skids
+#define TTOTALPIXELS 47 //TOTALNUMBER OF PIXELS ON THE tail
+
+// this defines the number of rows (horizontal strips) and
+// columns (number of LEDS on the longest strip
+#define COLUMNS 37 // THE NUMBER OF COLUMNS OF CANOPY LEDS
+#define ROWS 8 // THE NUMBER OF ROWS OF CANOPY LEDS
+
+// reference pixels
+#define TAILFINFIRST 28 // THE FIRST TAIL FIN PIXEL
+#define TAILFINLAST 46 // THE LAST TAIL FIN PIXEL
+#define SKIDSFIRST 4 // THE FIRST SKIDS PIXEL
+#define SKIDSLAST 17 // THE LAST SKIDS PIXEL
+#define BOOMFRONT 0 // THE FRONT (FIRST) TAIL BOOM PIXEL
+#define BOOMREAR 27 // THE REAR TAIL BOOM PIXEL
+#define UNDERFRONTFIRST 0 // THE FIRST FRONT UNDERCARRIAGE PIXEL
+#define UNDERFRONTLAST 3 // THE LAST FRONT UNDERCARRIAGE PIXEL
+#define UNDERREARFIRST 18 // THE FIRST REAR UNDERCARRIAGE PIXEL
+#define UNDERREARLAST 21 // THE LAST REAR UNDERCARRIAGE PIXEL
+
+// this defines the LED matrix on the LEFT canopy. The numbers are the addresses of the LEDs in rows from FRONT to BACK, running from TOP to BOTTOM
+// 999 refers to an LED that is not present
+uint16_t leftcanopy [][37] = {
+  {999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 336, 337, 338, 339, 340, 341, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 999, 999, 999, 999, 999, 308, 307, 306, 305, 304, 303, 302, 301, 300, 299, 298, 297, 296, 295, 294, 293, 292, 291, 290, 289, 288, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 999, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 999, 999, 999, 999, 999},
+  {999, 199, 198, 197, 196, 195, 194, 193, 192, 191, 190, 189, 188, 187, 186, 185, 184, 183, 182, 181, 180, 179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 999, 999},
+  {129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165},
+  {999, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 999, 34, 33, 32, 999, 6, 5, 4, 3, 2, 1, 0, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+};
+
+// this defines the LED matrix on the RIGHT canopy. The numbers are the addresses of the LEDs in rows from FRONT to BACK, running from TOP to BOTTOM
+// 999 refers to an LED that is not present
+uint16_t rightcanopy [][37] = {
+  {999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 335, 334, 333, 332, 331, 330, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 999, 999, 999, 999, 999, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 999, 259, 258, 257, 256, 255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236, 235, 234, 233, 999, 999, 999, 999, 999, 999},
+  {999, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 999, 999, 999},
+  {128, 127, 126, 125, 124, 123, 122, 121, 120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92},
+  {999, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+  {999, 999, 999, 29, 30, 31, 999, 7, 8, 9, 10, 11, 12, 13, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
+};
+
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // END OF CUSTOM SETUP ////////////////////////////////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // This code is written to run on a Feather M0 LORA 433Mhz
 // https://www.adafruit.com/product/3179
 
@@ -24,16 +86,6 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 #define SPIN 11 // THE PIN NUMBER ON THE BOARD THAT THE SKIDS DATA WIRE IS CONNECTED TO
 #define TPIN 10  // THE PIN NUMBER ON THE BOARD THAT THE TAIL DATA WIRE IS CONNECTED TO
 
-// this defines the number of rows (horizontal strips) and
-// columns (number of LEDS on the longest strip
-#define COLUMNS 37 // THE NUMBER OF COLUMNS OF CANOPY LEDS
-#define ROWS 8 // THE NUMBER OF ROWS OF CANOPY LEDS
-
-// for the three lines below it can be worth setting one more
-// than the actual number of pixels if the last pixel is behaving oddly
-#define CTOTALPIXELS 342 //TOTALNUMBER OF PIXELS ON THE CANOPY
-#define STOTALPIXELS 23 //total number of pixels on the skids
-#define TTOTALPIXELS 47 //TOTALNUMBER OF PIXELS ON THE tail
 
 // set up some useful colours for easy reference later
 #define BLUE 0x0000FF
@@ -62,18 +114,6 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 //  HUE_RED = 0, HUE_ORANGE = 32, HUE_YELLOW = 64, HUE_GREEN = 96,
 //  HUE_AQUA = 128, HUE_BLUE = 160, HUE_PURPLE = 192, HUE_PINK = 224
 
-// reference pixels
-#define TAILFINFIRST 28 // THE FIRST TAIL FIN PIXEL
-#define TAILFINLAST 46 // THE LAST TAIL FIN PIXEL
-#define SKIDSFIRST 4 // THE FIRST SKIDS PIXEL
-#define SKIDSLAST 17 // THE LAST SKIDS PIXEL
-#define BOOMFRONT 0 // THE FRONT (FIRST) TAIL BOOM PIXEL
-#define BOOMREAR 27 // THE REAR TAIL BOOM PIXEL
-#define UNDERFRONTFIRST 0 // THE FIRST FRONT UNDERCARRIAGE PIXEL
-#define UNDERFRONTLAST 3 // THE LAST FRONT UNDERCARRIAGE PIXEL
-#define UNDERREARFIRST 18 // THE FIRST REAR UNDERCARRIAGE PIXEL
-#define UNDERREARLAST 21 // THE LAST REAR UNDERCARRIAGE PIXEL
-
 CRGB cstrip[CTOTALPIXELS]; //setup the array for the canopy leds
 CRGB sstrip[STOTALPIXELS]; //setup the array for the skid leds
 CRGB tstrip[TTOTALPIXELS]; //setup the array for the tail leds
@@ -95,35 +135,6 @@ uint8_t rightspectrumtop[7];
 
 // This variable is used for the starlights function. One variable for each row.
 uint8_t starlights[ROWS];
-
-
-
-// this defines the LED matrix on the LEFT canopy. The numbers are the addresses of the LEDs in rows from FRONT to BACK, running from TOP to BOTTOM
-// 999 refers to an LED that is not present
-uint16_t leftcanopy [][37] = {
-  {999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 336, 337, 338, 339, 340, 341, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 999, 999, 999, 999, 999, 308, 307, 306, 305, 304, 303, 302, 301, 300, 299, 298, 297, 296, 295, 294, 293, 292, 291, 290, 289, 288, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 999, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 999, 999, 999, 999, 999},
-  {999, 199, 198, 197, 196, 195, 194, 193, 192, 191, 190, 189, 188, 187, 186, 185, 184, 183, 182, 181, 180, 179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 999, 999},
-  {129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165},
-  {999, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 999, 34, 33, 32, 999, 6, 5, 4, 3, 2, 1, 0, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-};
-
-// this defines the LED matrix on the RIGHT canopy. The numbers are the addresses of the LEDs in rows from FRONT to BACK, running from TOP to BOTTOM
-// 999 refers to an LED that is not present
-uint16_t rightcanopy [][37] = {
-  {999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 335, 334, 333, 332, 331, 330, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 999, 999, 999, 999, 999, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 999, 259, 258, 257, 256, 255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236, 235, 234, 233, 999, 999, 999, 999, 999, 999},
-  {999, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 999, 999, 999},
-  {128, 127, 126, 125, 124, 123, 122, 121, 120, 119, 118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92},
-  {999, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-  {999, 999, 999, 29, 30, 31, 999, 7, 8, 9, 10, 11, 12, 13, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999, 999},
-};
-
 
 uint8_t n; //just a handy reusable 8bit unsigned integer!
 uint8_t m; //just a handy reusable 8bit unsigned integer!
